aec25432044a2fff463492d9824cbc42d33e994b
samagra14 <samagra14@gmail.com>
2017-03-13 02:34:37 +0530

    ui changes

---
+++ app/src/main/java/com/sdsmdg/cognizance2017/fragments/EventDescription.java
@@ -0,0 +1,30 @@
+package com.sdsmdg.cognizance2017.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sdsmdg.cognizance2017.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EventDescription extends Fragment {
+
+
+    public EventDescription() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_event_description, container, false);
+    }
+
+}
